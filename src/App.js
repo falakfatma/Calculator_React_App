b3ec7f22e716c193f63a6af7fd0692278bb963ef
unknown <falakfatma510@gmail.com>
2024-06-04 00:42:58 +0530

    Basic UI Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 function App() {
+  const number = [1,2,3,4,5,6,7,8,9,0]
+  const operations = ['%','*','.','+','-']
+  const extraOperations = ['C','CE','Erase']
+  const [numbers,setNumbers] = useState(0)
+  const [operation,setOperation] = useState(0)
+  const [extraOperation,setExtraOperation] = useState(0)
+  const [value,setValue] = useState(0)
+
+  const numberFunc = (val) => {
+    console.log(val)
+    setValue(val)
+  }
+
   return (
-    <>
-    </>
+    <div className="Container">
+      <div className="main">
+        <input value={value}/>
+        <div>
+          {number.map((val,ind)=>{
+            return <button className="btn" key={ind} onClick={()=>numberFunc(val)}>{val}</button>
+          })}     
+          {operations.map((val,ind)=>{
+            return <button className="btn" key={ind}>{val}</button>
+          })}     
+          {extraOperations.map((val,ind)=>{
+            return <button className="btn" key={ind}>{val}</button>
+          })}     
+        </div>
+      </div>
+    </div>
   )
 }
 export default App
